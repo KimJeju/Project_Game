5dc5abcad96fed48b875c5955d00072e0f3735e8
Kimjeju <kingswingsex@gmail.com>
2022-02-13 17:19:38 +0900

    더블 링크드리스트 강의 오류로 인한 중단

---
+++ DoubleLinedListClass.val/DoubleLinedListClass.val/DoubleLinedListClass.val.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class CListNode
+{
+	CListNode() :
+		m_next(NULL),
+		m_prev(NULL)
+	{
+	}
+
+	~CListNode()
+	{
+	}
+
+
+private:
+	int m_iData;
+	CListNode* m_next;
+	CListNode* m_prev;
+};
+
+class CList
+{
+	CList()
+	{
+		m_pBegin = new CListNode;
+		m_pEnd = new CListNode;
+	}
+
+	~CList()
+	{
+	}
+private:
+	CListNode* m_pBegin();
+	CListNode* m_pEnd();
+	int iSize;
+};
+
+int main()
+{
+	return 0;
+}
+
